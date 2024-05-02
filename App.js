a068fc8400996d3ffe8c60eeb6e637369069c9fa
ghaith990000 <ghaith990000@gmail.com>
2024-05-02 21:01:06 +0300

    styling screen headers & backgrounds

--- App.js
+++ App.js
@@ -12,8 +12,22 @@ export default function App() {
     <>
       <StatusBar style="dark" />
       <NavigationContainer>
-        <Stack.Navigator initialRouteName='MealsCategories'>
-          <Stack.Screen name="MealsCategories" component={CategoriesScreen} />
+        <Stack.Navigator 
+          screenOptions={{
+           headerStyle: { backgroundColor: '#351401'},
+           headerTintColor: 'white',
+           contentStyle: {backgroundColor: '#3f2f25'}
+          }} 
+          initialRouteName='MealsCategories'>
+          <Stack.Screen 
+            name="MealsCategories" 
+            component={CategoriesScreen} 
+            options={{
+              title: 'All Categories',
+              headerStyle: { backgroundColor: '#351401'},
+              headerTintColor: 'white',
+              contentStyle: {backgroundColor: '#3f2f25'}
+            }} /> 
           <Stack.Screen name="MealsOverview" component={MealsOverviewScreen} />
         </Stack.Navigator>
       </NavigationContainer>
