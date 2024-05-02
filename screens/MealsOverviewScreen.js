3e47dba493badacde75a1a2f64f8d0a23d6bd230
ghaith990000 <ghaith990000@gmail.com>
2024-05-02 18:16:42 +0300

    displaying meals using the categoryId selected

--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,14 +1,26 @@
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, FlatList } from 'react-native'
 import React from 'react'
 import { MEALS } from '../data/dummyData'
+import MealItem from '../components/MealItem';
 
 
 
 const MealsOverviewScreen = ({ route }) => {
     const catId = route.params.categoryId;
+
+    const displayedMeals = MEALS.filter((mealItem) => {
+        return mealItem.categoryIds.indexOf(catId) >= 0;
+    });
+
+    const renderMealItem = (itemData) => {
+        return <MealItem title={itemData.item.title} />
+    }
   return (
     <View style={styles.container}>
-      <Text>MealsOverviewScreen - {catId}</Text>
+      <FlatList 
+        data={displayedMeals} 
+        keyExtractor={(item) => item.id} 
+        renderItem={renderMealItem} />
     </View>
   )
 }
