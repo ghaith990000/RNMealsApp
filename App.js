4613c1a4de6cef705f003f5b4e46fe21c5d5284f
ghaith990000 <ghaith990000@gmail.com>
2024-05-04 13:42:50 +0300

    configure Context Provider

--- App.js
+++ App.js
@@ -7,7 +7,8 @@ import MealsOverviewScreen from './screens/MealsOverviewScreen';
 import MealDetailsScreen from './screens/MealDetailsScreen';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import FavoritesScreen from './screens/FavoritesScreen';
-
+import {Ionicons } from '@expo/vector-icons'
+import FavoritesContextProvider from './store/context/favorites-context';
 const Stack = createNativeStackNavigator();
 const Drawer = createDrawerNavigator();
 
@@ -17,13 +18,26 @@ const DrawerNavigator = () =>  {
   screenOptions={{
     headerStyle: { backgroundColor: '#351401'},
     headerTintColor: 'white',
-    sceneContainerStyle: {backgroundColor: '#3f2f25'}
+    sceneContainerStyle: {backgroundColor: '#3f2f25'},
+    drawerContentStyle: { backgroundColor: '#351401'},
+    drawerInactiveTintColor: 'white',
+    drawerActiveTintColor: '#351401',
+    drawerActiveBackgroundColor: '#e4baa1'
    }} 
   >
     <Drawer.Screen name="Categories" component={CategoriesScreen} options={{
-      title: 'All Catgories'
+      title: 'All Catgories',
+      drawerIcon: ({color, size}) => (
+        <Ionicons name='list' color={color} size={size} />
+      )
     }} />
-    <Drawer.Screen name="Favorites" component={FavoritesScreen} />
+    <Drawer.Screen name="Favorites" component={FavoritesScreen}
+      options={{
+        drawerIcon: ({color, size}) => (
+          <Ionicons name='star' color={color} size={size} />
+        )
+      }}
+    />
   </Drawer.Navigator>
 }
 
@@ -31,6 +45,7 @@ export default function App() {
   return (
     <>
       <StatusBar style="light" />
+      <FavoritesContextProvider>
       <NavigationContainer>
         <Stack.Navigator 
           screenOptions={{
@@ -60,7 +75,7 @@ export default function App() {
           />
         </Stack.Navigator>
       </NavigationContainer>
-    
+      </FavoritesContextProvider>
     </>
   );
 }
