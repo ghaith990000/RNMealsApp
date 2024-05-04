54c0276bb69db20822ef9f440a93f49bf8b4337a
ghaith990000 <ghaith990000@gmail.com>
2024-05-04 14:37:05 +0300

    solve bug when removing from favorites list

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,13 +1,14 @@
 import { View, Text, StyleSheet } from 'react-native'
 import React, { useContext } from 'react'
 import MealsList from '../components/MealsList/MealsList'
-import { FavoritesContext } from '../store/context/favorites-context'
 import { MEALS } from '../data/dummyData'
+import { useSelector } from 'react-redux'
 
 const FavoritesScreen = () => {
-  const favoriteMealsCtx = useContext(FavoritesContext)
+  // const favoriteMealsCtx = useContext(FavoritesContext)
+  const favoriteMealsIds = useSelector(state => state.favoriteMeals.ids);
 
-  const favoriteMeals = MEALS.filter(meal => favoriteMealsCtx.ids.includes(meal.id));
+  const favoriteMeals = MEALS.filter(meal => favoriteMealsIds.includes(meal.id));
   if(favoriteMeals.length === 0){
     return <View style={styles.rootContainer}>
       <Text style={styles.text}>You have no favorite meals yet.</Text>
